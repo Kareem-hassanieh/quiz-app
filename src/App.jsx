c02afc4126cdf298745587da551be559b9627d51
Kareem-hassanieh <kareemalh19@gmail.com>
2024-10-14 10:07:49 +0300

    adding the colors of the wrong and correct answers also adding styling and images

--- src/App.jsx
+++ src/App.jsx
@@ -2,72 +2,138 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
   const questions = [
     {
-      question: "what is the capital of columbia",
-      options: ["Berlin", "Bogatta", "paris", "Beirut"],
-      answer: "Bogatta"
+      question: "what is the capital of the Germany?",
+      options: ["Berlin", "Lima", "paris", "Beirut"],
+      answer: "Berlin",
+      images: ['src/assets/pexels-stuthnagyniki-1128416.jpg', 'src/assets/pexels-shvets-2570063.jpg']
     },
 
 
     {
 
-      question: "what is the longest tower in the world",
-      options: ["burj khalifa", "paris tower", "Beirut tower", "shanghai tower"],
-      answer: "shanghai tower"
+      question: "what is the capital of the Japan?",
+      options: ["Beijing", "Tokyo", "Seoul", "Moscow"],
+      answer: "Tokyo",
+      images: ['src/assets/pexels-apasaric-1510595.jpg', 'src/assets/pexels-nickkwanhk-2614818.jpg']
+    },
+
+    {
+      question: "what is the capital of the Australlia?",
+      options: ["cabul", "canberra", "aukland", "sydney"],
+      answer: "canberra",
+      images: ['src/assets/pexels-luis-sosa-1141354972-22866400.jpg', 'src/assets/pexels-mark-direen-622749-17582203.jpg']
     }
+
+
 
   ]
 
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const [currentImageIndex, setQurrentImageIndex] = useState(0)
-  const [hasAnswered,setHasAnswered]=useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+  const [hasAnswered, setHasAnswered] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   function handleOptionClick(option) {
 
-    if(!hasAnswered){
+    if (!hasAnswered) {
+      setSelectedOption(option);
       if (option === questions[currentQuestion].answer) {
         setScore(score + 1);
       }
-      setHasAnswered(true);
-      }
+    }
 
+    setHasAnswered(true);
 
-
-  
   }
 
-  function handleNextQuestion(){
-    setCurrentQuestion(currentQuestion+1);
+  function handleNextQuestion() {
+    setCurrentQuestion(currentQuestion + 1);
     setHasAnswered(false);
   }
 
+  function handleLeftClick() {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? 1 : 0))
+  }
+  function handleRightClick() {
+    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? 1 : 0))
+  }
 
-return (
-  <>
+  
 
-    <div className="question-container">
-      <h2>{questions[currentQuestion].question}</h2>
+  return (
+    <>
+
+
+
+
+
+
+      <div className='image-container'>
+
+        <button onClick={handleLeftClick} className='left-arrow'>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+
+
+        <img src={questions[currentQuestion].images[currentImageIndex]}></img>
+
+
+
+
+
+
+
+
+
+        <button onClick={handleRightClick} className='right-arrow'>
+
+          <FontAwesomeIcon icon={faArrowRight} />
+
+
+        </button>
+
+
+      </div>
+
+      <h1>{questions[currentQuestion].question}</h1>
 
 
       <div className="options-container">
 
         {questions[currentQuestion].options.map((option, index) => (
-          <button key={index} onClick={() => handleOptionClick(option)}  disabled={hasAnswered}>{option}</button>
-        ))}
+          <button key={index} onClick={() => handleOptionClick(option)} disabled={hasAnswered}
+          style={{
+            backgroundColor:
+              hasAnswered && option === questions[currentQuestion].answer
+                ? 'green'
+                : hasAnswered && option === selectedOption
+                ? 'red'
+                : ''
+          }}
+
+          
+
+          >{option}</button>
+        ))
+
+        }
+
+        <button id="next-button" onClick={handleNextQuestion} disabled={!hasAnswered || currentQuestion === questions.length - 1}>Next</button>
       </div>
-      <button className="next-button" onClick={handleNextQuestion}>Next</button>
-
-      <p>Score:{score}</p>
 
 
 
-    </div>
+
+      <h2>Score:{score}/3</h2>
 
 
 
@@ -75,8 +141,12 @@ return (
 
 
 
-  </>
-)
+
+
+
+
+    </>
+  )
 }
 
 export default App
